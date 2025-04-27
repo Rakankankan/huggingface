fe00df46a90e2ecdd54e7f9c201b1cebe667b0b6
Rakankankan <rakanazizu894@gmail.com>
2025-04-27 19:52:54 +0700

    main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ TELEGRAM_CHAT_ID = "5721363432"
 NOTIFICATION_INTERVAL = 300  # 5 menit dalam detik
 ALERT_COOLDOWN = 60  # 1 menit cooldown untuk notifikasi langsung
 CAMERA_URL = "http://192.168.1.12:81/stream"  # URL kamera ESP32-CAM
-GEMINI_API_KEY = "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"
-GEMINI_MODEL = "google/gemini-flash-1.5"
+GEMINI_API_KEY = "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"
+GEMINI_MODEL = "deepseek/deepseek-chat-v3-0324:free"
 
 # --- STYLE ---
 st.markdown("""
